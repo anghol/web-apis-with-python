c004579aa7e86310550cd71e5d708742abad2b26
anghol <angelkholicheva@gmail.com>
2024-04-18 20:28:22 +0300

    Simple API to greet user

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 # Intitialise the app
 app = Flask(__name__)
@@ -6,12 +6,15 @@ app = Flask(__name__)
 # Define what the app does
 @app.get("/greet")
 def index():
-    """
-    TODO:
-    1. Capture first name & last name
-    2. If either is not provided: respond with an error
-    3. If first name is not provided and second name is provided: respond with "Hello Mr <second-name>!"
-    4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
-    5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
-    """
-    return jsonify("TODO")
+    fname = request.args.get("fname")
+    lname = request.args.get("lname")
+
+    if not fname and not lname:
+        response = jsonify({"status": "error"})
+    elif fname and not lname:
+        response = jsonify({"data": f"Hello, {fname}!"})
+    elif lname and not fname:
+        response = jsonify({"data": f"Hello, Mr. {lname}!"})
+    else:
+        response = jsonify({"data": f"Is your name {fname} {lname}?"})
+    return response
